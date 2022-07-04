dd73117cdb2cc0c47fd48013a793ce2ee9027cc2
Alcides <ayunierto@gmail.com>
2022-07-04 06:56:42 -0500

    Add express middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const morgam = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', 3000);
+
+// Middlewares
+app.use(morgam('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename(req, file, cb) {
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+});
+app.use(multer({storage}).single('image'));
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // Start the server
 app.listen(app.get('port'), () => {
