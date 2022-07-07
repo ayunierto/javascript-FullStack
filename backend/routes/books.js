a0de724cd4c724c8208fa565d9326c1a38d43254
Alcides <ayunierto@gmail.com>
2022-07-07 06:09:12 -0500

    Creating Rest API

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,6 +1,23 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => res.json({ text: 'Hello world' }));
+const Books = require('../models/Books');
+
+router.get('/', async (req, res) => {
+    const books = await Books.find();
+    res.json(books);
+});
+
+router.post('/', async (req, res) => {
+    const {title, author, isbn} = req.body;
+    const newBook = new Books(title, author, isbn);
+    await newBook.save();
+    res.send({message: "Book Saved"});
+});
+
+router.delete('/:id', async (req, res) => {
+    await Books.findByIdAndDelete(req.params.id);
+    res.json({message: 'Book deleted'});
+});
 
 module.exports = router;
