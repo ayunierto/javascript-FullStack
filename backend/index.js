e2af5cf0ff9b530da0055c7aff6d6e22b005358b
Alcides <ayunierto@gmail.com>
2022-07-12 06:59:08 -0500

    Service Clases, fetch and ajax, add imagePath

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const morgam = require('morgan');
 const multer = require('multer');
 const path = require('path');
+const cors = require('cors');
 
 // Initializations
 const app = express();
@@ -26,6 +27,7 @@ app.use(multer({storage}).single('image'));
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/api/books',require('./routes/books'));
